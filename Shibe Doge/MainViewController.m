3adc69cd4058f3e4a6e028c4930d64d678185d72
sebastian andreasson <sebastian.andreasson93@gmail.com>
2013-10-27 18:03:30 +0100

    wow such commit

--- Shibe Doge/MainViewController.m
+++ Shibe Doge/MainViewController.m
@@ -8,7 +8,13 @@
 
 #import "MainViewController.h"
 
-@interface MainViewController ()
+@interface MainViewController (){
+    UIImageView *animatedDoge;
+    UILabel *title;
+    
+    UIButton *photoButton;
+    UIButton *libraryButton;
+}
 
 @end
 
@@ -17,7 +23,34 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    [self.navigationController setNavigationBarHidden:YES];
+    [self initViews];
+    
+}
+
+- (void)initViews{
+    title = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 320, 60)];
+    [title setText:@"Shibe Doge"];
+    [title setBackgroundColor:[UIColor clearColor]];
+    [title setTextColor:[UIColor magentaColor]];
+    [title setTextAlignment:NSTextAlignmentCenter];
+    [title setFont:[UIFont fontWithName:@"Comic Sans MS Bold" size:24]];
+    [self.view addSubview:title];
+    
+    photoButton = [[UIButton alloc] initWithFrame:CGRectMake(170, self.view.frame.size.height-64, 130, 44)];
+    [photoButton setBackgroundColor:[UIColor orangeColor]];
+    [photoButton setTitle:@"such photo" forState:UIControlStateNormal];
+    [photoButton addTarget:self action:@selector(goToNextPage) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:photoButton];
+    
+    libraryButton = [[UIButton alloc] initWithFrame:CGRectMake(20, self.view.frame.size.height-64, 130, 44)];
+    [libraryButton setBackgroundColor:[UIColor orangeColor]];
+    [libraryButton setTitle:@"such library" forState:UIControlStateNormal];
+    [self.view addSubview:libraryButton];
+}
+
+- (void)goToNextPage{
+    [self performSegueWithIdentifier:@"suchSegue" sender:nil];
 }
 
 - (void)didReceiveMemoryWarning
