40bde60aa2c1db9bb95882d9522d653c7af41af6
johancato <johan-cato@hotmail.com>
2013-10-27 17:37:35 +0100

    Added SecondViewController and new view to storyboard, such progress!

---
+++ Shibe Doge/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  Shibe Doge
+//
+//  Created by Johan Cato on 2013-10-27.
+//  Copyright (c) 2013 Sebastian Andreasson Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
---
+++ Shibe Doge/SecondViewController.m
@@ -0,0 +1,40 @@
+//
+//  SecondViewController.m
+//  Shibe Doge
+//
+//  Created by Johan Cato on 2013-10-27.
+//  Copyright (c) 2013 Sebastian Andreasson Apps. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+
+	}
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+	self.view setBackgroundColor:<#(UIColor *)#>
+
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
